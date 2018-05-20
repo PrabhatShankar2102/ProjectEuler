5822ca204dad6c5343e334c85e08b7457c7a4bae
PrabhatShankar2102 <prabhat.shnkr@gmail.com>
2018-05-20 14:17:36 -0400

    Commiting the 1st Solution of Euler

---
+++ src/Multiplesof3and5.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Multiplesof3and5 {
+	public ArrayList<Integer> list ;
+	public Multiplesof3and5(ArrayList<Integer> list) {
+		this.list = list ;
+	}
+	
+	public void multiple(int n) {
+		for (int i =0;i<n ; i++) {
+			if(i%3 == 0 || i%5 == 0) {
+				list.add(i);
+			}
+		}
+	}
+	public void sum(ArrayList<Integer> arr) {
+		int sum = 0 ;
+		for (Integer integer : arr) {
+			sum += integer;
+		}
+		System.out.println(sum);
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList();
+		Multiplesof3and5 obj = new Multiplesof3and5(list);
+		int n = 1000 ;
+		obj.multiple(n);
+		obj.sum(list);
+		
+	}
+}
