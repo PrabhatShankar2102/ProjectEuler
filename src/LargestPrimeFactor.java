cdda258a1fbf0ba753fced6552f2725806648bae
PrabhatShankar2102 <prabhat.shnkr@gmail.com>
2018-05-23 12:28:52 -0400

    3rd problem of Euler removed unnecessary details

--- src/LargestPrimeFactor.java
+++ src/LargestPrimeFactor.java
@@ -13,40 +13,27 @@ import java.util.List;
 public class LargestPrimeFactor {
 
 	public List<Long> factors;// = new ArrayList<Integer>();
-	public List<Long> primeFactors;// = new ArrayList<Integer>();
+	//public List<Long> primeFactors;// = new ArrayList<Integer>();
 	
 	public LargestPrimeFactor() {
 		factors = new ArrayList<Long>();
-		primeFactors = new ArrayList<Long>();
+		//primeFactors = new ArrayList<Long>();
 	}
 	
-	//get all the factors first
 	public long factors(long n) {
 		 long maxPrime = -1;
 		 
-	        // Print the number of 2s
-	        // that divide n
 	        while (n % 2 == 0) {
 	            maxPrime = 2;
 	 
-	            // equivalent to n /= 2
 	            n >>= 1;
 	        }
-	 
-	        // n must be odd at this point,
-	        // thus skip the even numbers
-	        // and iterate only for odd
-	        // integers
 	        for (int i = 3; i <= Math.sqrt(n); i += 2) {
 	            while (n % i == 0) {
 	                maxPrime = i;
 	                n = n / i;
 	            }
 	        }
-	 
-	        // This condition is to handle
-	        // the case when n is a prime
-	        // number greater than 2
 	        if (n > 2)
 	            maxPrime = n;
 	 
